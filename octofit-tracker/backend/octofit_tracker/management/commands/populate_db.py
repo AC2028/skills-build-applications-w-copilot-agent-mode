d48ec2ce9d54a2833d89e9a9292e639aba4c7683
AC2028 <130003972+AC2028@users.noreply.github.com>
2025-04-08 20:30:57 +0000

    changes 3

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,40 +1,40 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
-from bson import ObjectId
+from django.conf import settings
+from pymongo import MongoClient
 from datetime import timedelta
+from bson import ObjectId
 
 class Command(BaseCommand):
-    help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
+    help = 'Populate the database with test data for users, teams, activity, leaderboard, and workouts'
 
     def handle(self, *args, **kwargs):
-        # Clear existing data
-        User.objects.all().delete()
-        Team.objects.all().delete()
-        Activity.objects.all().delete()
-        Leaderboard.objects.all().delete()
-        Workout.objects.all().delete()
+        # Connect to MongoDB
+        client = MongoClient(settings.DATABASES['default']['HOST'], settings.DATABASES['default']['PORT'])
+        db = client[settings.DATABASES['default']['NAME']]
+
+        # Drop existing collections
+        db.users.drop()
+        db.teams.drop()
+        db.activity.drop()
+        db.leaderboard.drop()
+        db.workouts.drop()
 
         # Create users
         users = [
             User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
             User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
             User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
-            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
+            User(_id=ObjectId(), username='crashoverride', email='crashoverride@hmhigh.edu', password='crashoverridepassword'),
             User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
         ]
-        print('Creating users...')
         User.objects.bulk_create(users)
-        print(f'{len(users)} users created.')
 
         # Create teams
-        team1 = Team(_id=ObjectId(), name='Blue Team')
-        team2 = Team(_id=ObjectId(), name='Gold Team')
-        print('Creating teams...')
-        team1.save()
-        team2.save()
-        team1.members.add(users[0], users[1])
-        team2.members.add(users[2], users[3], users[4])
-        print('Teams created and members added.')
+        team = Team(_id=ObjectId(), name='Blue Team')
+        team.save()
+        for user in users:
+            team.members.add(user)
 
         # Create activities
         activities = [
@@ -44,9 +44,7 @@ class Command(BaseCommand):
             Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=timedelta(minutes=30)),
             Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=timedelta(hours=1, minutes=15)),
         ]
-        print('Creating activities...')
         Activity.objects.bulk_create(activities)
-        print(f'{len(activities)} activities created.')
 
         # Create leaderboard entries
         leaderboard_entries = [
@@ -56,9 +54,7 @@ class Command(BaseCommand):
             Leaderboard(_id=ObjectId(), user=users[3], score=85),
             Leaderboard(_id=ObjectId(), user=users[4], score=80),
         ]
-        print('Creating leaderboard entries...')
         Leaderboard.objects.bulk_create(leaderboard_entries)
-        print(f'{len(leaderboard_entries)} leaderboard entries created.')
 
         # Create workouts
         workouts = [
@@ -68,8 +64,6 @@ class Command(BaseCommand):
             Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
             Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
         ]
-        print('Creating workouts...')
         Workout.objects.bulk_create(workouts)
-        print(f'{len(workouts)} workouts created.')
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
